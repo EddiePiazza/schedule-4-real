e8be45d1bb3f072a36da7cd8d6d4d4005ee4b346
Eddie Piazza <ptakx@hotmail.com>
2026-04-08 03:48:42 +0200

    Publish: web v1.111.2, supervisor v1.24.1, schema v11, ingest v1.9.1, mosquitto v1.2.2, proxy v1.2.2, relay v1.6.2, retention v1.0.5

---
+++ schedule-4-real/.output/public/_nuxt/DqmCkuDj.js
@@ -0,0 +1,1 @@
+import{l as e}from"./D2bk6x87.js";const r=e(!1);function o(){return{sidebarOpen:r}}export{o as u};
--- schedule-4-real/.output/public/_nuxt/gpnDp2S7.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./DUX82XZS.js";const r=e(!1);function o(){return{sidebarOpen:r}}export{o as u};
